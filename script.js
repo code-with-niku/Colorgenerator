b7aed87974e740bced3faeef45a25475ad760a7f
Nikhil Mishra <153975081+code-with-niku@users.noreply.github.com>
2024-06-02 14:51:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+  const color = document.querySelector("#color");
+  var text = document.querySelector("#text");
+  const generate = document.querySelector("#generate");
+  const copy = document.querySelector("#copy");
+  var randomcolor = "";
+
+  generate.addEventListener("click",()=>{
+     const random = Math.floor(Math.random()*0xffffff);
+     randomcolor ="#" + random.toString(16);
+    color.style.backgroundColor = randomcolor;
+    text.innerHTML=randomcolor;
+  })
+  copy.addEventListener("click",()=>{
+    navigator.clipboard.writeText(randomcolor);
+  })
+  
+  
+  const mode = document.querySelector("#mode");
+  mode.addEventListener("click",() => {
+  document.body.classList.toggle("darkmode");
+  if(document.body.classList.contains("darkmode")){
+  mode.src="moon.png";
+  }else{
+   mode.src="sun.png";
+  }
+  text.classList.toggle("darkmode");
+  copy.classList.toggle("darkmode");
+  generate.classList.toggle("darkmode");
+})
+
